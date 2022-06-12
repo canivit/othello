32fe711dfe7045275a30113166451b57013689bf
Can Ivit <canivit@users.noreply.github.com>
2022-06-12 17:44:21 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "othello";
+
+  outputs = { self, nixpkgs }: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+    };
+  in {
+    devShell.x86_64-linux = pkgs.mkShell {
+      name = "othello";
+      buildInputs = with pkgs; [
+        dotnet-sdk_6
+      ];
+    };
+  };
+}
