1105e41db8d647bbb73a4fa7d8d7066f67f1ac88
Can Ivit <canivit@users.noreply.github.com>
2022-06-30 23:01:11 -0400

    start frontend

--- flake.nix
+++ flake.nix
@@ -10,7 +10,12 @@
       name = "othello";
       buildInputs = with pkgs; [
         dotnet-sdk_6
+        nodejs
+        nodePackages.yarn
       ];
+      shellHook = ''
+        export PATH=$PWD/src/frontend/node_modules/.bin:$PATH
+      '';
     };
   };
 }
